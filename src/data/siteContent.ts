251af17b6f7ae416cef40502b7f95e11b85d7d1c
Melinca <149501274+Melinca@users.noreply.github.com>
2026-07-06 15:22:55 -0400

    Update hero image URLs in siteContent.ts

--- src/data/siteContent.ts
+++ src/data/siteContent.ts
@@ -18,7 +18,7 @@ export function imageUrl(url: string) {
 export const siteImages = {
   logo: imageUrl('https://drive.google.com/file/d/1HZ4gyCTO9N8zt-3nKhkpmTv7Oqj_RSkn/view?usp=drive_link'),
   hero: imageUrl(
-    'https://images.pexels.com/photos/3997991/pexels-photo-3997991.jpeg?auto=compress&cs=tinysrgb&w=1920'
+    'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/30/10/b8/35/vv07-aesthetics.jpg?w=700&h=-1&s=1'
   ),
 };
 
